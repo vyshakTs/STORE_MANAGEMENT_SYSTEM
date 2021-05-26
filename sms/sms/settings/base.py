9f09e1bd6ca70f8f0b2037886f30da997615480a
Abhiram TS <vyshak3k@gmail.com>
2021-05-26 19:32:01 +0530

    bug fix

--- sms/sms/settings/base.py
+++ sms/sms/settings/base.py
@@ -68,7 +68,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     # all auth
-    # 'django.contrib.sites',
+    'django.contrib.sites',
     # 'allauth',
     # 'allauth.account',
     # 'allauth.socialaccount',
