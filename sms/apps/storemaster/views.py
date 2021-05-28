8017da112cb73dd846105ad794a7b110e5d23558
Abhiram TS <vyshak3k@gmail.com>
2021-05-29 00:00:42 +0530

    Added new templates

--- sms/apps/storemaster/views.py
+++ sms/apps/storemaster/views.py
@@ -43,5 +43,7 @@ class StoreProfileView(generic.TemplateView):
             return redirect('store_profile')          
         return super().get(request, *args, **kwargs)
     
-    # def get_context_data(self, **kwargs):
-    #     pass
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        return context
