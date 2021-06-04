221402f1aa869400fcd2a128533cb22c50d03b2d
Abhiram TS <vyshak3k@gmail.com>
2021-06-04 18:48:13 +0530

    bug fix

--- config/settings/production.py
+++ config/settings/production.py
@@ -8,4 +8,4 @@ ALLOWED_HOSTS = ['.herokuapp.com']
 #     "default": env.dj_db_url("DATABASE_URL")
 # }
 # Parse database configuration from $DATABASE_URL
-DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
